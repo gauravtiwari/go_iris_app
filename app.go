3e4e7c3340b1c29df86926962d3ae9118214cb8f
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-07-22 10:04:07 +0100

    First go web app in iris

---
+++ app.go
@@ -0,0 +1,30 @@
+package main
+import (
+  "github.com/kataras/iris"
+  "github.com/iris-contrib/template/django"
+)
+
+func main() {
+  iris.UseTemplate(
+    django.New(),
+  ).Directory("./templates", ".html")
+
+  iris.Get("/", index)
+  iris.Get("/hello", hello)
+  iris.Listen(":3000")
+}
+
+func hello(ctx *iris.Context){
+  ctx.Render(
+    "hello.html",
+    map[string]interface{}{"Message": "Hello World"},
+    iris.RenderOptions{"gzip":true},
+  )
+}
+
+func index(ctx *iris.Context){
+  ctx.JSON(
+    iris.StatusOK,
+    map[string]string{"message": "Hello World"},
+  )
+}
