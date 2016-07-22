28d14a091435564fbf90cb60226f0e8928199c70
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-07-22 10:24:08 +0100

    Add basic example with net http

--- app.go
+++ app.go
@@ -1,30 +1,31 @@
 package main
+
 import (
-  "github.com/kataras/iris"
-  "github.com/iris-contrib/template/django"
+	"github.com/iris-contrib/template/django"
+	"github.com/kataras/iris"
 )
 
 func main() {
-  iris.UseTemplate(
-    django.New(),
-  ).Directory("./templates", ".html")
+	iris.UseTemplate(
+		django.New(),
+	).Directory("./templates", ".html")
 
-  iris.Get("/", index)
-  iris.Get("/hello", hello)
-  iris.Listen(":3000")
+	iris.Get("/", index)
+	iris.Get("/hello", hello)
+	iris.Listen(":3000")
 }
 
-func hello(ctx *iris.Context){
-  ctx.Render(
-    "hello.html",
-    map[string]interface{}{"Message": "Hello World"},
-    iris.RenderOptions{"gzip":true},
-  )
+func hello(ctx *iris.Context) {
+	ctx.Render(
+		"hello.html",
+		map[string]interface{}{"Message": "Hello World"},
+		iris.RenderOptions{"gzip": true},
+	)
 }
 
-func index(ctx *iris.Context){
-  ctx.JSON(
-    iris.StatusOK,
-    map[string]string{"message": "Hello World"},
-  )
+func index(ctx *iris.Context) {
+	ctx.JSON(
+		iris.StatusOK,
+		map[string]string{"message": "Hello World"},
+	)
 }
