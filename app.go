492f5c3cbec7ea77a1fcc01f8e0f054a716440bf
kataras <kataras2006@hotmail.com>
2017-11-17 20:16:51 +0200

    update to latest iris + some tips

--- app.go
+++ app.go
@@ -1,31 +1,45 @@
 package main
 
 import (
-	"github.com/iris-contrib/template/django"
 	"github.com/kataras/iris"
 )
 
+// Serve using a host:port form.
+var addr = iris.Addr("localhost:3000")
+
 func main() {
-	iris.UseTemplate(
-		django.New(),
-	).Directory("./templates", ".html")
+	app := iris.New()
 
-	iris.Get("/", index)
-	iris.Get("/hello", hello)
-	iris.Listen(":3000")
+	// Register the templates/**.html as django and reload them on each request
+	// so changes can be reflected, set to false on production.
+	app.RegisterView(iris.Django("./templates", ".html").Reload(true))
+
+	// GET: http://localhost:3000
+	app.Get("/", index)
+
+	// GET: http://localhost:3000/profile/myname/article/42
+	app.Get("/profile/{name:string}/article/{id:int}", iris.Gzip, article)
+
+	// Now listening on: http://localhost:3000
+	// Application started. Press CTRL+C to shut down.
+	app.Run(addr)
 }
 
-func hello(ctx *iris.Context) {
-	ctx.Render(
-		"hello.html",
-		map[string]interface{}{"Message": "Hello World"},
-		iris.RenderOptions{"gzip": true},
+func article(ctx iris.Context) {
+	// retrieve the dynamic path parameters.
+	var (
+		name         = ctx.Params().Get("name")
+		articleID, _ = ctx.Params().GetInt("id")
 	)
+
+	// set the template's binded values.
+	ctx.ViewData("Name", name)
+	ctx.ViewData("ArticleID", articleID)
+
+	// finally, render the template.
+	ctx.View("article.html")
 }
 
-func index(ctx *iris.Context) {
-	ctx.JSON(
-		iris.StatusOK,
-		map[string]string{"message": "Hello World"},
-	)
+func index(ctx iris.Context) {
+	ctx.JSON(iris.Map{"message": "Hello World"})
 }
--- readme.md
+++ readme.md
@@ -1,8 +1,19 @@
-#### A basic web app built in Iris web framework for Golang
+# A basic web app built in Iris web framework for Go
+
+![overview screen](readme_screen.png)
+
+## Getting started
+
+1. Install Go (Golang)
+    * Using [Brew](https://brew.sh/).
+    * Or by navigating to https://golang.org/dl.
+2. Download & Install [Iris](https://iris-go.com) using go get: `go get -u github.com/kataras/iris`
 
 ```bash
-brew install go
-cd $GOPATH/src/
-git clone git@github.com:gauravtiwari/go_iris_app.git
-go run app.go
+$ brew install go
+$ go get -u github.com/kataras/iris
+$ cd $GOPATH/src/
+$ git clone https://github.com/iris-contrib/go_iris_app.git
+$ go run app.go
 ```
+
